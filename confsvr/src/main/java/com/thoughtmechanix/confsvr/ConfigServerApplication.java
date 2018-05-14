0a80445e7998288dc35035af452763efe1d991a7
Shangfei <shangfei.cao@covata.com>
2018-05-14 17:38:49 +1000

    init repo

---
+++ confsvr/src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2018 Covata Limited or its affiliates
+ *
+ * Information contained within this file cannot be copied,
+ * distributed and/or practised without the written consent of
+ * Covata Limited or its affiliates.
+ */
+
+package com.thoughtmechanix.confsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class);
+    }
+}
---
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -0,0 +1,50 @@
+package com.thoughtmechanix.licenses.controllers;
+
+import com.thoughtmechanix.licenses.model.License;
+import com.thoughtmechanix.licenses.services.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@RestController
+@RequestMapping(value="v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+    @Autowired
+    private LicenseService licenseService;
+
+    @GetMapping("/{licenseId}")
+    public License getLicenses( @PathVariable("organizationId") String organizationId,
+                                @PathVariable("licenseId") String licenseId) {
+
+        //return licenseService.getLicense(licenseId);
+        return new License()
+            .withId(licenseId)
+            .withOrganizationId(organizationId)
+            .withProductName("Teleco")
+            .withLicenseType("Seat");
+    }
+
+    @PutMapping("{licenseId}")
+    public String updateLicenses( @PathVariable("licenseId") String licenseId) {
+        return String.format("This is the put");
+    }
+
+    @PostMapping("{licenseId}")
+    public String saveLicenses( @PathVariable("licenseId") String licenseId) {
+        return String.format("This is the post");
+    }
+
+    @DeleteMapping("{licenseId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public String deleteLicenses( @PathVariable("licenseId") String licenseId) {
+        return String.format("This is the Delete");
+    }
+}
